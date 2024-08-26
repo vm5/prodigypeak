1fb74cbcea1b3144ad9a091fd35c09ff694e277e
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-26 15:17:25 +0530

    Update usercodes.jsx

--- src/usercodes.jsx
+++ src/usercodes.jsx
@@ -2,7 +2,7 @@
 
 export const userCodes = {
     student1: { code: 'student1234', name: 'Vanshika Khemka', role: 'student' },
-    teacher1: { code: 'teacher1234', name: 'Aarushi Kumar', role: 'teacher' },
+    teacher1: { code: 'teacher1234', name: 'Varsha Gandhi', role: 'teacher' },
     // Add more users here
   };
   
