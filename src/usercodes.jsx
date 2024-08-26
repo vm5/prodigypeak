3ff72e80bfc991f33b5a0ff1ae30e314ddf2c785
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-26 15:14:48 +0530

    Create usercodes.jsx

---
+++ src/usercodes.jsx
@@ -0,0 +1,8 @@
+// userCodes.js
+
+export const userCodes = {
+    student1: { code: 'student1234', name: 'Vanshika Khemka', role: 'student' },
+    teacher1: { code: 'teacher1234', name: 'Aarushi Kumar', role: 'teacher' },
+    // Add more users here
+  };
+  
